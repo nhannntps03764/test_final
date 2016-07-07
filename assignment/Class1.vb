8ac47ca52b2121f15cf0fed16ea3b2409af4a365
test_final <danhthien260280@gmail.com>
2016-07-07 08:03:08 +0700

    load source

---
+++ assignment/Class1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Public Class Class1
+
+    Public Function Loadsanpham() As DataSet
+        Dim chuoiketnoi As String = "workstation id=ps03764.mssql.somee.com;packet size=4096;user id=ps03764_SQLLogin_1;pwd=mhzqynwrs5;data source=ps03764.mssql.somee.com;persist security info=False;initial catalog=ps03764"
+        Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim LoadSP As New SqlDataAdapter("select SANPHAM.MASP as 'Mã sản phẩm',SANPHAM.TENSP as 'Tên sản phẩm', LOAISANPHAM.MALOAI as 'Mã Loại', LOAISANPHAM.TENLOAI as 'Tên Loại',SANPHAM.SOLUONG as 'Số lượng' from SANPHAM inner join LOAISANPHAM on SANPHAM.MASP = LOAISANPHAM.MASP", conn)
+        Dim db As New DataSet
+        conn.Open()
+        LoadSP.Fill(db)
+        conn.Close()
+        Return db
+    End Function
+End Class
